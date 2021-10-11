30b8938ec03a40aac9e1796e26581e0da130e23a
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-10-11 14:52:40 -0600

    comit2

--- SegundoLab/SegundoLab/Service/IPersona.cs
+++ SegundoLab/SegundoLab/Service/IPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SegundoLab.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,10 @@ using System.Threading.Tasks;
 
 namespace SegundoLab.Service
 {
-    interface IEdad
+    interface IPersona
     {
-        void Create(Entidad E);
-        
+        void Guardar(persona persona );
+        List<persona> listardatos();
+    
     }
 }
