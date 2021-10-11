0cba39dc7d9c9c6ef042915ef73452a5c404752e
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-10-11 16:45:32 -0600

    Comit4

--- Laboratorio2/Laboratorio2/Form1.cs
+++ Laboratorio2/Laboratorio2/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Laboratorio2.Data;
+using Laboratorio2.Negocio;
+using Laboratorio2.Vista;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +28,23 @@ namespace Laboratorio2
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            persona per = new persona();
+            per.nombrePersona = textBox1.Text;
+            per.edadPersona = Convert.ToInt32(textBox2.Text);
+            per.descripcionPersona = textBox3.Text;
+            ClsNPersona clsNPersona = new ClsNPersona();
+            clsNPersona.Guardar(per);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmVista formulario = new FrmVista();
+            formulario.Show();
         }
     }
 }
