8c7bd3b5dd0334a31356c159caaf945dba9e570c
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-10-11 16:14:59 -0600

    ActualizacionNegocio

--- Laboratorio2/Laboratorio2/Negocio/ClsNPersona.cs
+++ Laboratorio2/Laboratorio2/Negocio/ClsNPersona.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Laboratorio2.Data;
+using Laboratorio2.Repository;
 
 namespace Laboratorio2.Negocio
 {
-    class ClsNPersona
+    class ClsNPersona : ClsRPersona
     {
+        public void Guardar(persona per)
+        {
+            Crear(per);
+        }
+        public List<persona> ListFinal()
+        {
+            List<persona> list = ListofDataofTablePersona();
+            return list;
+        }
+
+
     }
+
+
 }
